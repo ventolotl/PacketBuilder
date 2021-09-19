f426744b27baffc250a1947c3f59ba25499796a8
ventolotl <ventolotl@users.noreply.github.com>
2021-09-19 12:05:40 +0200

    Initial commit

---
+++ src/main/kotlin/de/ventolotl/packetbuilder/PacketBuilder.kt
@@ -0,0 +1,132 @@
+package de.ventolotl.packetbuilder
+
+import com.comphenix.protocol.PacketType
+import com.comphenix.protocol.ProtocolLibrary
+import com.comphenix.protocol.ProtocolManager
+import com.comphenix.protocol.events.PacketContainer
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+
+private val protocol: ProtocolManager = ProtocolLibrary.getProtocolManager()
+
+class PacketBuilder(private val packet: PacketContainer) {
+    companion object {
+        /** Constructs the [PacketBuilder] with the given [type]. */
+        @JvmStatic
+        fun with(type: PacketType, creation: Builder.() -> Unit): PacketBuilder {
+            val packet = protocol.createPacket(type)
+            creation(Builder(packet))
+            return PacketBuilder(packet)
+        }
+    }
+
+    /** Sends the [packet] to the given [player]. */
+    fun send(player: Player) {
+        protocol.sendServerPacket(player, this.packet)
+    }
+
+    /** Sends the [packet] to the given [players]. */
+    fun send(players: Iterable<Player>) {
+        players.forEach { player -> send(player) }
+    }
+
+    /**
+     * Sends the [packet] to all players except
+     * the given [player].
+     */
+    fun sendExcept(player: Player) {
+        val players = Bukkit.getOnlinePlayers()
+        players
+            .filter { other -> other != player }
+            .forEach { receiver -> send(receiver) }
+    }
+}
+
+/** Sends a sequence of [PacketBuilder]s to the given [player]. */
+fun Iterable<PacketBuilder>.send(player: Player) {
+    this.forEach { packet -> packet.send(player) }
+}
+
+/** Sends a sequence of [PacketBuilder]s to the given [players]. */
+fun Iterable<PacketBuilder>.send(players: Iterable<Player>) {
+    players.forEach(this::send)
+}
+
+/**
+ * Sends a sequence of [PacketBuilder]s to
+ * all players except the given [player].
+ */
+fun Iterable<PacketBuilder>.sendExcept(player: Player) {
+    this.forEach { packet -> packet.sendExcept(player) }
+}
+
+/** Defines various [IndexEntry]s used to determine the next index. */
+enum class IndexEntry {
+    INT,
+    DOUBLE,
+    BYTE,
+    CHAT_COMPONENT,
+    BOOLEAN,
+    INT_ARRAY,
+    DATA_WATCHER,
+    ITEM,
+    ITEM_SLOT,
+    UUID,
+}
+
+/** Indexes fields orders with various [IndexEntry]s. */
+class Indexer {
+    private val map = hashMapOf<IndexEntry, Int>()
+
+    /**
+     * Indexes the next field of the given [entry].
+     *
+     * If the first entry is `INT`, it returns 0.
+     * If the next entry is `INT`, it returns 1.
+     * If the third entry is `BYTE`, it returns 0.
+     */
+    fun next(entry: IndexEntry): Int {
+        map[entry] = map.getOrPut(entry) { 0 } + 1
+        return map[entry]?.minus(1) ?: 0
+    }
+}
+
+class Builder(
+    packet: PacketContainer,
+) {
+    private val indexer = Indexer()
+
+    val byte = ByteWriter(packet, indexer)
+    val intArray = IntArrayWriter(packet, indexer)
+    val boolean = BooleanWriter(packet, indexer)
+    val entityId = IntWriter(packet, indexer)
+    val entityType = IntWriter(packet, indexer)
+    val armorPosition = IntWriter(packet, indexer)
+    val itemSlot = ItemSlotWriter(packet, indexer)
+    val uuid = UUIDWriter(packet, indexer)
+    val position = PositionWriter.create(packet, indexer)
+    val rotation = RotationWriter(packet, indexer)
+    val rotationYaw = RotationHeadWriter(packet, indexer)
+    val dataWatcher = DataWatcherWriter(packet, indexer)
+    val item = ItemWriter(packet, indexer)
+    val chatComponent = ChatComponentWriter(packet, indexer)
+}
+
+infix fun <T : Any> Writer<T>.to(value: T) {
+    invoke(value)
+}
+
+open class Writer<T : Any>(
+    private val entry: IndexEntry,
+    internal val packet: PacketContainer,
+    private val indexer: Indexer,
+) {
+    open operator fun invoke(value: T) {
+        val modifier = packet.modifier
+        modifier.write(indexer.next(entry), value)
+    }
+
+    protected fun next(): Int {
+        return indexer.next(entry)
+    }
+}
---
+++ src/main/kotlin/de/ventolotl/packetbuilder/Writer.kt
@@ -0,0 +1,180 @@
+package de.ventolotl.packetbuilder
+
+import com.comphenix.protocol.events.PacketContainer
+import com.comphenix.protocol.utility.MinecraftVersion
+import com.comphenix.protocol.wrappers.EnumWrappers
+import com.comphenix.protocol.wrappers.WrappedChatComponent
+import com.comphenix.protocol.wrappers.WrappedDataWatcher
+import org.bukkit.inventory.ItemStack
+import org.bukkit.util.Vector
+import java.util.*
+
+class BooleanWriter(
+    packet: PacketContainer,
+    indexer: Indexer,
+) : Writer<Boolean>(IndexEntry.BOOLEAN, packet, indexer) {
+    override fun invoke(value: Boolean) {
+        packet.booleans.write(next(), value)
+    }
+}
+
+class IntWriter(
+    packet: PacketContainer,
+    indexer: Indexer,
+) : Writer<Int>(IndexEntry.INT, packet, indexer) {
+    override fun invoke(value: Int) {
+        packet.integers.write(next(), value)
+    }
+}
+
+class IntArrayWriter(
+    packet: PacketContainer,
+    indexer: Indexer,
+) : Writer<Array<Int>>(IndexEntry.INT_ARRAY, packet, indexer) {
+    override fun invoke(value: Array<Int>) {
+        packet.integerArrays.write(next(), value.toIntArray())
+    }
+
+    private fun Array<Int>.toIntArray(): IntArray {
+        val intArray = IntArray(this.size)
+        for ((i, value) in this.withIndex()) {
+            intArray[i] = value
+        }
+        return IntArray(this.size)
+    }
+
+    operator fun invoke(vararg values: Int) {
+        packet.integerArrays.write(next(), values)
+    }
+}
+
+class ByteWriter(
+    packet: PacketContainer,
+    indexer: Indexer,
+) : Writer<Byte>(IndexEntry.BYTE, packet, indexer) {
+    override fun invoke(value: Byte) {
+        packet.bytes.write(next(), value)
+    }
+}
+
+class ItemSlotWriter(
+    packet: PacketContainer,
+    indexer: Indexer,
+) : Writer<EnumWrappers.ItemSlot>(IndexEntry.ITEM_SLOT, packet, indexer) {
+    override fun invoke(value: EnumWrappers.ItemSlot) {
+        packet.itemSlots.write(next(), value)
+    }
+}
+
+class UUIDWriter(
+    packet: PacketContainer,
+    indexer: Indexer,
+) : Writer<UUID>(IndexEntry.UUID, packet, indexer) {
+    override fun invoke(value: UUID) {
+        packet.uuiDs.write(next(), value)
+    }
+}
+
+abstract class PositionWriter(
+    entry: IndexEntry,
+    packet: PacketContainer,
+    indexer: Indexer,
+) : Writer<Vector>(entry, packet, indexer) {
+    companion object {
+        @JvmStatic
+        fun create(
+            packet: PacketContainer,
+            indexer: Indexer,
+        ): PositionWriter {
+            return when (MinecraftVersion.COMBAT_UPDATE.atOrAbove()) {
+                true -> ModernPositionWriter(packet, indexer)
+                false -> LegacyPositionWriter(packet, indexer)
+            }
+        }
+    }
+}
+
+class ModernPositionWriter(
+    packet: PacketContainer,
+    indexer: Indexer,
+) : PositionWriter(IndexEntry.DOUBLE, packet, indexer) {
+    override fun invoke(value: Vector) {
+        packet.doubles
+            .write(next(), value.x)
+            .write(next(), value.y)
+            .write(next(), value.z)
+    }
+}
+
+private const val POSITION_CONVERT_FACTOR = 32.0
+
+class LegacyPositionWriter(
+    packet: PacketContainer,
+    indexer: Indexer,
+) : PositionWriter(IndexEntry.INT, packet, indexer) {
+    override fun invoke(value: Vector) {
+        packet.integers
+            .write(next(), (value.x * POSITION_CONVERT_FACTOR).floor())
+            .write(next(), (value.y * POSITION_CONVERT_FACTOR).floor())
+            .write(next(), (value.z * POSITION_CONVERT_FACTOR).floor())
+    }
+
+    private fun Double.floor(): Int {
+        val i = this.toInt()
+        return if (this < i.toDouble()) i - 1 else i
+    }
+}
+
+private const val ROTATION_CONVERT_FACTOR = 256.0f / 360.0f
+
+class RotationHeadWriter(
+    packet: PacketContainer,
+    indexer: Indexer,
+) : Writer<Float>(IndexEntry.BYTE, packet, indexer) {
+    override fun invoke(value: Float) {
+        packet.bytes.write(next(), (value * ROTATION_CONVERT_FACTOR).toInt().toByte())
+    }
+}
+
+class RotationWriter(
+    packet: PacketContainer,
+    indexer: Indexer,
+) : Writer<Rotation>(IndexEntry.BYTE, packet, indexer) {
+    override fun invoke(value: Rotation) {
+        packet.bytes
+            .write(next(), (value.yaw * ROTATION_CONVERT_FACTOR).toInt().toByte())
+            .write(next(), (value.pitch * ROTATION_CONVERT_FACTOR).toInt().toByte())
+    }
+}
+
+data class Rotation(val yaw: Float, val pitch: Float)
+
+class DataWatcherWriter(
+    packet: PacketContainer,
+    indexer: Indexer,
+) : Writer<WrappedDataWatcher>(IndexEntry.DATA_WATCHER, packet, indexer) {
+    override fun invoke(value: WrappedDataWatcher) {
+        val modifier = packet.watchableCollectionModifier
+        modifier.write(next(), value.watchableObjects)
+    }
+}
+
+class ItemWriter(
+    packet: PacketContainer,
+    indexer: Indexer,
+) : Writer<ItemStack>(IndexEntry.ITEM, packet, indexer) {
+    override fun invoke(value: ItemStack) {
+        val modifier = packet.itemModifier
+        modifier.write(next(), value)
+    }
+}
+
+class ChatComponentWriter(
+    packet: PacketContainer,
+    indexer: Indexer,
+) : Writer<WrappedChatComponent>(IndexEntry.CHAT_COMPONENT, packet, indexer) {
+    override fun invoke(value: WrappedChatComponent) {
+        val modifier = packet.chatComponents
+        modifier.write(next(), value)
+    }
+}
